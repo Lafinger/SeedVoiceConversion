c9f308c210a735a2622054f571411fcd5c9a9c47
Lafinger <jc904034764@gmail.com>
2026-01-23 20:49:38 +0800

    添加环境安装脚本

---
+++ 环境安装脚本.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+setlocal
+set "ROOT_DIR=%~dp0"
+cd /d "%ROOT_DIR%"
+
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+
+uv venv --python=3.10
+uv init --python=3.10
+uv pip install -r requirements-win.txt --torch-backend cu128 --prerelease allow
+
+pause
